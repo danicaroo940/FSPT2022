1b1402aaf16d299127c36a4bf46d6c13f4dae65a
ADA <adalawson96@gmail.com>
2022-10-10 19:42:10 +0200

    java

---
+++ ALH/java/main.js
@@ -0,0 +1,129 @@
+//EJERCICIO 1: Impares por consola 1-20
+
+// const limit = 20;
+// // let i = 3;
+// for (let i = 1; i<=limit; i++){
+//     if(i%2===0){
+//         continue; 
+//     }
+//     console.log('Mi indice es' +i);
+// }
+
+// // Answer:
+// const maxNumber= 20;
+// // 2 porque quiero tener impares
+// const divisor = 2;
+
+// for (let i = 0; i < maxNumber; i++) {
+//     const leftOver = i % divisor;
+//     const is Odd = leftOver === 1;
+//     if(isOdd){
+//         console.log(i + 'is odd')
+//     } 
+// }
+
+// // opcion optima:
+//  for (let i = 0; i < maxLaps; i = i = 2) { 
+//     // i = i+2 === i+2; es lo mismo
+//     console.log('----' + 1 'is odd');
+//  }
+
+// EJERCICIO 2: crear 10 numeros del fibonacci
+
+// const Laps= 10;
+// let firstNumber= 0;
+// let secondNumber= 1;
+
+// console.log(firstNumber); 
+// console.log(secondNumber);
+
+// for (let i = 0; i < Laps; i++) {
+//     let result = firstNumber + secondNumber;
+//     console.log(result);
+//     firstNumber = secondNumber;
+//     secondNumber = result;  
+// }
+
+// EJERCICIO 3: Calendario Octubre 
+
+// const weekDay = [
+//     'sabado' , //0
+//     'domingo', //1
+//     'Lunes', //2
+//     'Martes' , //3
+//     'Miercoles' , //4
+//     'jueves' , //5
+//     'viernes' , //6
+//     ]; 
+    
+//     let daynumber = 5
+
+// for (let i = 1; i <= 31; i++) {
+//     console.log(weekday(daynumber); // 1-31
+//     daynumber++
+
+//     if(daynumber>6){
+//         daynumber = 0;
+//     }
+    // let Weekday = 
+    // console.log(weekDay[i-1]); // lunes-domingo
+    // secondNumber = result;  
+// }
+
+// EJERCICIO 4: Calculadora (funciones)// add, substract, multiply, divide, AC
+
+// function add(n1, n2){
+//     const resultone = n1 + n2;
+//     return resultone;
+// }
+
+// console.log (
+//     add(1 , 2)
+// )
+
+// function subtract(n1, n2){
+//     const resulttwo = n1 - n2;
+//     return resulttwo;
+// }
+
+// console.log (
+//     subtract(2 , 1)
+// )
+
+// function multiply(n1, n2){
+//     const resultthree = n1 * n2;
+//     return resultthree;
+// }
+
+// console.log (
+//     multiply(3 , 2)
+// )
+
+// function divide(n1, n2) {
+//     const resultfour = n1 / n2;
+//     return resultfour;
+// }
+
+// console.log (
+//     divide(4 , 2)
+// )
+
+// EJERCICIOS DE https://www.youtube.com/watch?v=jS4aFq5-91M
+
+// let myAge= 26
+// let humanDogRatio= 7
+// let myDogAge = myAge *humanDogRatio
+
+// console.log (myDogAge)
+// let bonusPoints= 50
+// bonusPoints = bonusPoints + 100 - 25 + 70
+// console.log (bonusPoints)
+
+// Array
+const groceries = [ 'banana', 'apple', 'orange', 'pear']
+console.log(groceries)
+console.log(groceries[1])
+// groceries.push('cookie')
+// console.log(groceries)
+
+
