93605967cc8d4a89cb3326fc331d573a88f95244
alejosdo <84529562+alejosdo@users.noreply.github.com>
2022-10-14 01:14:01 +0200

    Sin medir distancias yyyy mi sombra quedará de aquel amor

--- AAL/proyecto1.js
+++ AAL/proyecto1.js
@@ -1,65 +1,12 @@
 
-function money (a){
-    console.log(a)
-}
-
- if (a === 500) {
-    a % 500;
-}
-
-if (a === 200) {
-    a % 200;
-}
-
-if (a === 100) {
-    a % 100
-}
-
-if (a === 50) {
-    a % 50
-}
-
-if (a === 20) {
-    a % 20
-}
-
-if (a === 10) {
-    a % 10
-}
-
-if (a === 5) {
-    a % 5
-}
-
-if (a === 2) {
-    a % 2
-}
-
-if (a === 1) {
-    a % 1
-}
-
-if (a === 0.5) {
-    a % 0.5
-}
-
-if (a === 50) {
-    a % 0.2
-}
-
-if (a === 0.1) {
-    a % 0.1
-}
-
-if (a === 0.05) {
-    a % 0.05
-}
-
-if (a === 0.02) {
-    a % 0.02
-}
-
-if (a === 0.01) {
-    a % 0.01
+function money (amount) {
+    const bills = [500,200,100,50,20,10,5,2,1]
+    let remainder = amount;
+    for( let bill of bills){
+         amountBill_total = remainder/bill;
+         amountBill = Math.floor(amountBill_total);
+         remainder = remainder - (amountBill*bill);
+         console.log("The amount of " + bill + " bills in amount is "+ amountBill);
+    }
 }
 // money('Hola')
