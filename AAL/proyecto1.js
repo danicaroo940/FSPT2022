20310ecbbd8b75109bbcbe9c1d7024348301f747
alejosdo <84529562+alejosdo@users.noreply.github.com>
2022-10-14 01:28:50 +0200

    WAAA

--- AAL/proyecto1.js
+++ AAL/proyecto1.js
@@ -2,11 +2,13 @@
 function money (amount) {
     const bills = [500,200,100,50,20,10,5,2,1]
     let remainder = amount;
+    console.log( "------------Calculation for this amount: " + amount)
     for( let bill of bills){
          amountBill_total = remainder/bill;
          amountBill = Math.floor(amountBill_total);
          remainder = remainder - (amountBill*bill);
-         console.log("The amount of " + bill + " bills in amount is "+ amountBill);
+         
+         let denomination= bill>10?"bills":"coins"; 
+         console.log("The amount of " + bill + " " + denomination + " in amount is "+ amountBill);
     }
 }
-// money('Hola')
