e15f517bca21027cad7105fb58ad4cb97458abb6
laumd23 <laumd.23@gmail.com>
2022-10-14 08:55:27 +0200

    cambios importantes

--- LMD/Js/main.js
+++ LMD/Js/main.js
@@ -1,23 +1,25 @@
 function atm(amount) {
   
   let importe = amount
-  let monedas = [500, 200, 100, 50, 20, 10, 5, 2, 1, 0.50, 0.20, 0.02, 0.01];
-  let cambio = [0, 0,0,0,0,0,0,0,0,0,]
+  let currency = [500, 200, 100, 50, 20, 10, 5, 2, 1, 0.50, 0.20, 0.02, 0.01];
+  let change = [0, 0,0,0,0,0,0,0,0,0,]
 
-  console.log('Vas a sacar ' + amount + ' €');
+  console.log('Monto a retirar ' + amount + ' €');
 
-  for (let i = 0; i < monedas.length; i++) {
+  for (let i = 0; i < currency.length; i++) {
 
-    if (importe >= monedas[i]) {
-      cambio[i] = (importe - (importe % monedas[i])) / monedas[i];
-      importe = (importe % monedas[i]).toFixed(2);
+    if (importe >= currency[i]) {
+      change[i] = (importe - (importe % currency[i])) / currency[i];
+      importe = (importe % currency[i]).toFixed(2);
       // importe = (importe % monedas[i])
 
-      if (monedas[i] < 5) {
-        console.log('Se han retirado ' + cambio[i] + ' monedas de ' + monedas[i] + ' €');
+      if (currency[i] < 5) {
+        console.log('Se han retirado ' + change[i] + ' monedas de ' + currency[i] + ' €');
+        console.log('Has sacado ' + (currency[i])*change[i] + ' €');
         console.log('Quedan por sacar ' + importe + ' €');
       } else {
-        console.log('Se han retirado ' + cambio[i] + ' billetes de ' + monedas[i] + ' €');
+        console.log('Se han retirado ' + change[i] + ' billetes de ' + currency[i] + ' €');
+        console.log('Has sacado ' + (currency[i])*change[i] + ' €');
         console.log('Quedan por sacar ' + importe + ' €');
       }
     }
