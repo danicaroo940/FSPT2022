bcaa69336dcf0a8f11941dd8d22ae67fec38484f
ChrisMA <christian.meneses.ls@gmail.com>
2022-10-19 18:44:10 +0200

    update

--- CMA/Codigo/Chat/chat.js
+++ CMA/Codigo/Chat/chat.js
@@ -1,43 +1,40 @@
 function sendText(input) {
-    let inputString = '';
-    switch (input) {
-        case 1:
-            inputString = 'text-input1';
-            break;
-        case 2:
-            inputString = 'text-input2';
-            break;
-    };
+	let inputString = '';
+	switch (input) {
+		case 1:
+			inputString = 'text-input1';
+			break;
+		case 2:
+			inputString = 'text-input2';
+			break;
+	};
 
-    const chatContainer = document.getElementById('chat-container');
-    const textInput = document.getElementById(inputString);
-    const text = textInput.value;
+	const chatContainer = document.getElementById('chat-container');
+	const textInput = document.getElementById(inputString);
+	const text = textInput.value;
 
-    if (text != '') {
-        const message = document.createElement('div');
-        switch (input) {
-            case 1:
-                message.className = 'message-in';
-                break;
-            case 2:
-                message.className = 'message-out';
-                break;
-        };
-        message.innerHTML = text;
-        chatContainer.appendChild(message);    
-    };
+	if (text != '') {
+		const message = document.createElement('div');
+		switch (input) {
+			case 1:
+				message.className = 'message-in';
+				break;
+			case 2:
+				message.className = 'message-out';
+				break;
+		};
+		message.innerHTML = text;
+		chatContainer.appendChild(message);    
+	};
 
-    textInput.value = '';
-    textInput.focus();
-    chatContainer.scrollTo(0, chatContainer.scrollHeight);
+	textInput.value = '';
+	textInput.focus();
+	chatContainer.scrollTo(0, chatContainer.scrollHeight);
 }
 
 function handleKeyPress(e,input){
-    const key = e.keyCode || e.which;
-     if (key == 13 & input == 1){
-        sendText(1);
-     }
-     if (key == 13 & input == 2){
-        sendText(2);
-     }
+	const key = e.keyCode || e.which;
+		if (key == 13){
+			sendText(input);
+		}
 }
